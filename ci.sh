0dfd2a878b971641f28f0ec69f508a5671ae1bdd
jaydeep <jaydeepk@gmail.com>
2026-06-15 17:54:51 +0530

    Honor SEND_REPORT in web-frontend CI script

--- ci.sh
+++ ci.sh
@@ -2,3 +2,11 @@
 
 npm install
 npm test
+
+if [[ -n "${SEND_REPORT:-}" ]]; then
+  java -jar ~/.specmatic/specmatic-enterprise.jar send-report \
+    --repo-id="$(gh api 'repos/{owner}/{repo}' --jq .id)" \
+    --repo-name="$(gh repo view --json name -q .name)" \
+    --repo-url="$(gh repo view --json url --jq .url)" \
+    --branch-name main
+fi
